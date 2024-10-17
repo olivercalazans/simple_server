5c41843ba5e800be6f57bcf4a9f91f3cb23653da
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-10-17 01:33:35 -0300

    Add files via upload

---
+++ strategy.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+class Strategy(ABC):
+    @abstractmethod
+    def execute(self, server, arguments=None):
+        pass
+
+
+class Command_List_Strategy(Strategy):
+    def execute(self, _server, _client_port:int, _arguments:str):
+        _result = _server.get_command_list()
+        return ('svc', _result)
+
+
+class Private_Message_Strategy(Strategy):
+    def execute(self, _server, _client_port:int, _arguments:str):
+        _result = _server.prepare_private_message(_client_port, _arguments)
+        return ('pvt', _result)
+    
+
+class Broadcast_Message_Strategy(Strategy):
+    def execute(self, _server, _client_port:int, _arguments:str):
+        _result = _server.prepare_broadcast_message(_client_port, _arguments)
+        return ('bdc', _result)
+
+
+class File_List_On_The_Server_Strategy(Strategy):
+    def execute(self, _server, _client_port:int, _arguments:str):
+        _result = _server.get_file_list_on_the_server()
+        return ('svc', _result)
+
+
+class Delete_File_Strategy(Strategy):
+    def execute(self, _server, _client_port:int, _arguments:str):
+        _result = _server.delete_file(_arguments)
+        return ('svc', _result)
+
+
+class Send_File_Name_And_Size_Strategy(Strategy):
+    def execute(self, _server, _client_port:int, _arguments:str):
+        _result = _server.get_file_information(_arguments)
+        return ('svc', _result)
+
+
+class Send_File_To_Client_Strategy(Strategy):
+    def execute(self, _server, _client_port:int, _arguments:str):
+        _result = _server.separete_file_infomation(_arguments)
+        return ('sfl', _result)
+    
+
+class Receive_File_From_Client_Strategy(Strategy):
+    def execute(self, _server, _client_port:int, _arguments:str):
+        _result = _server.separete_file_infomation(_arguments)
+        return ('recv_file', _result)
